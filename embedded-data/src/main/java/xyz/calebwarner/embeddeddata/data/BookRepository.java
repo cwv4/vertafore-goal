126eaa09b3088e3d822d982835d45420654d0119
Caleb Warner <cwarner@vertafore.com>
2024-10-23 09:52:19 -0400

    Added enumeration to embedded data app

--- embedded-data/src/main/java/xyz/calebwarner/embeddeddata/data/BookRepository.java
+++ embedded-data/src/main/java/xyz/calebwarner/embeddeddata/data/BookRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import xyz.calebwarner.embeddeddata.model.Book;
+import xyz.calebwarner.embeddeddata.model.Genre;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -11,6 +12,8 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findByTitle(String title);
+
+    List<Book> findByGenre(Genre genre);
 
     @Query("SELECT b FROM Book b WHERE b.publishDate > :date")
     List<Book> findByPublishedDateAfter(@Param("date") LocalDate date);
--- hotel-app/src/main/java/xyz/calebwarner/hotelapp/models/Position.java
+++ hotel-app/src/main/java/xyz/calebwarner/hotelapp/models/Position.java
@@ -3,6 +3,7 @@ package xyz.calebwarner.hotelapp.models;
 public enum Position {
     CONCIERGE, FRONT_DESK, HOUSEKEEPING, SECURITY;
 
+    @Override
     public String toString() {
         return switch (this) {
             case CONCIERGE -> "Concierge";
