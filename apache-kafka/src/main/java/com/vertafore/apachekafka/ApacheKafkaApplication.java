63cc16c95cad7cb0c31a3d9d5c77618b8eb6b623
Caleb Warner <cwarner@vertafore.com>
2025-01-09 15:06:08 -0500

    Added apache-kafka module that showcases a simple use of messaging in an app

---
+++ apache-kafka/src/main/java/com/vertafore/apachekafka/ApacheKafkaApplication.java
@@ -0,0 +1,17 @@
+package com.vertafore.apachekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApacheKafkaApplication.class, args);
+    }
+
+    /*
+     * Kafka video:
+     * https://www.youtube.com/watch?v=inrQUHLPFd4&list=PLGRDMO4rOGcNLwoack4ZiTyewUcF6y6BU
+     */
+}
---
+++ apache-kafka/src/main/java/com/vertafore/apachekafka/controller/MessageController.java
@@ -0,0 +1,25 @@
+package com.vertafore.apachekafka.controller;
+
+import com.vertafore.apachekafka.KafkaProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/kafka")
+public class MessageController {
+
+    private final KafkaProducer kafkaProducer;
+
+    public MessageController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> publish(@RequestParam("message") String message) {
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok(String.format("Message sent to the topic: %s", message));
+    }
+}
